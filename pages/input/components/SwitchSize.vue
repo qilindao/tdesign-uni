2a8dac343040f1beae034010b19d891ae9cecc97
joycezhang <787027175@qq.com>
2023-08-01 16:26:56 +0800

    ✨(demo):   Switch 开关

---
+++ pages/input/components/SwitchSize.vue
@@ -0,0 +1,26 @@
+<template>
+	<t-cell title="大尺寸 32">
+		<template #rightIcon>
+			<t-switch size="large" :default-value="true" />
+		</template>
+	</t-cell>
+	<t-cell title="中尺寸 28">
+		<template #rightIcon>
+			<t-switch :default-value="true" />
+		</template>
+	</t-cell>
+	<t-cell title="小尺寸 24">
+		<template #rightIcon>
+			<t-switch :default-value="true" size="small" />
+		</template>
+	</t-cell>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+	.t-cell+.t-cell {
+		margin-top: 16px;
+	}
+</style>
---
+++ pages/input/switch.vue
@@ -0,0 +1,78 @@
+<template>
+	<view class="input-example">
+		<view class="demo-title">Switch 开关</view>
+		<view class="demo-desc">用于控制某个功能的开启和关闭。</view>
+		<TDemo title="01 组件类型" desc="基础开关">
+			<t-cell title="基础开关" :bordered="false">
+				<template #rightIcon>
+					<t-switch v-model="checked"  />
+				</template>
+			</t-cell>
+		</TDemo>
+		<TDemo desc="带描述开关">
+			<t-cell title="带文字开关">
+				<template #rightIcon>
+					<t-switch :default-value="true" :label="['开', '关']" />
+				</template>
+			</t-cell>
+
+			<t-cell title="带图标开关" :bordered="false">
+				<template #rightIcon>
+					<t-switch :default-value="true" :icon="['check', 'close']" />
+				</template>
+			</t-cell>
+
+		</TDemo>
+		<TDemo desc="自定义颜色开关">
+			<t-cell title="自定义颜色开关">
+				<template #rightIcon>
+					<t-switch :default-value="true" class="custom-color" />
+				</template>
+			</t-cell>
+		</TDemo>
+		<TDemo title="02 组件状态">
+			<SwitchStatus />
+		</TDemo>
+
+		<TDemo title="03 组件样式" desc="开关尺寸">
+			<SwitchSize />
+		</TDemo>
+
+	</view>
+</template>
+
+<script>
+	import {
+		ref,
+		defineComponent
+	} from "vue"
+	import TDemo from '@/components/TDemo'
+	import SwitchStatus from './components/SwitchStatus'
+	import SwitchSize from './components/SwitchSize'
+
+	export default defineComponent({
+		name: "DemoSwitch",
+		components: {
+			TDemo,
+			SwitchStatus,
+			SwitchSize
+		},
+		setup() {
+			const checked = ref(true);
+			const handleChange1 = (val) => {
+				checked.value = val;
+			}
+
+			return {
+				checked,
+				handleChange1
+			}
+		}
+	})
+</script>
+
+<style scoped>
+	.custom-color {
+		--td-switch-checked-color: #00a870;
+	}
+</style>
