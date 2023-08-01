c60dd41f0a068bfcf686b58397cad097f62aa1bc
joycezhang <787027175@qq.com>
2023-08-01 16:27:35 +0800

    ✨(demo):   Radio 单选框

---
+++ pages/input/components/RadioSpecial.vue
@@ -0,0 +1,84 @@
+<template>
+	<t-radio-group v-model:value="radio">
+		<view v-for="(item, index) in 3" :key="index" :class="`card ${radio == index ? 'card--active' : ''}`">
+			<t-icon v-if="radio == index" name="check" class="card__icon" />
+			<t-radio :value="index" label=" " content="描述信息描述信息描述信息描述信息描述信息" icon="none" borderless />
+		</view>
+	</t-radio-group>
+
+	<view class="tdesign-mobile-demo-block__summary" style="margin-left: 16px; margin-top: 24px">横向卡片单选框</view>
+
+	<t-radio-group v-model:value="radio1" class="horizontal-box">
+		<view v-for="(item, index) in 3" :key="index" :class="`card ${radio1 == index ? 'card--active' : ''}`">
+			<t-icon v-if="radio1 == index" name="check" class="card__icon" />
+			<t-radio :value="index" label="单选" icon="none" borderless />
+		</view>
+	</t-radio-group>
+</template>
+<script setup>
+	import {
+		ref
+	} from 'vue';
+
+	const radio = ref(0);
+	const radio1 = ref(0);
+</script>
+<style lang="less" scoped>
+	.card {
+		position: relative;
+		margin: 32rpx;
+		border-radius: 12rpx;
+		overflow: hidden;
+		box-sizing: border-box;
+		border: 3rpx solid #fff;
+	}
+
+	.card--active {
+		border-color: #0052d9;
+	}
+
+	.card--active::after {
+		content: '';
+		display: block;
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 0;
+		border: 14px solid #0052d9;
+		border-bottom-color: transparent;
+		border-right-color: transparent;
+	}
+
+	.card__icon {
+		color: #fff;
+		position: absolute;
+		left: 1.5px;
+		top: 1.5px;
+		z-index: 1;
+	}
+
+	/* 横向布局 */
+	.horizontal-box {
+		width: calc(100% - 64rpx);
+		display: flex;
+		align-items: center;
+		margin: 32rpx;
+	}
+
+	.horizontal-box .card {
+		flex: 1;
+		margin: 0;
+	}
+
+	.horizontal-box .card+.card {
+		margin-left: 24rpx;
+	}
+
+	.horizontal-box .card::after {
+		border-width: 12px;
+	}
+
+	.horizontal-box .card__icon {
+		font-size: 12px;
+	}
+</style>
---
+++ pages/input/radio.vue
@@ -0,0 +1,138 @@
+<template>
+	<view class="input-example">
+		<view class="demo-title">Radio 单选框</view>
+		<view class="demo-desc">用于在预设的一组选项中执行单项选择，并呈现选择结果。</view>
+		<TDemo title="01 组件类型" desc="纵向单选框">
+			<t-radio-group @change="groupChangeFn" v-model="curRadio" :options="options" />
+		</TDemo>
+		<TDemo desc="横向单选框">
+			<t-radio-group class="boxRadioWrap" v-model="curRadio" borderless @change="groupChangeFn">
+				<t-radio :block="false" name="radio" :value="1" label="单选标题" />
+				<t-radio :block="false" name="radio" :value="2" label="单选标题" />
+				<t-radio :block="false" name="radio" :value="3" label="单选标题" />
+			</t-radio-group>
+		</TDemo>
+
+		<TDemo title="02 组件状态" desc="单选框状态">
+			<t-radio-group v-model="radio1" disabled :options="statusOptions" class="radio-group-demo space" />
+		</TDemo>
+
+		<TDemo title="03 组件样式" desc="勾选样式">
+			<t-radio :default-checked="true" icon="line" label="单选" />
+
+			<view class="gutter" style="height: 32rpx"></view>
+
+			<t-radio :default-checked="true" icon="dot" label="单选" />
+		</TDemo>
+		<TDemo desc="勾选显示位置">
+			<t-radio label="单选" default-checked />
+
+			<view style=" height: 32rpx;"></view>
+
+			<t-radio label="单选" placement="right" default-checked />
+		</TDemo>
+		<TDemo desc="非通栏单选样式">
+			<t-radio-group class="theme-card" v-model="radio2" @change="groupChangeFn">
+				<t-radio name="radio3" value="1" label="单选" />
+				<t-radio name="radio3" value="2" label="单选" />
+				<t-radio name="radio3" value="4" label="单选标题多行单选标题多行单选标题多行单选标题多行单选标题多行" />
+			</t-radio-group>
+		</TDemo>
+
+		<TDemo title="04 特殊样式" desc="纵向卡片单选框">
+			<RadioSpecial />
+		</TDemo>
+	</view>
+</template>
+
+<script>
+	import {
+		ref,
+		defineComponent
+	} from "vue"
+	import TDemo from '@/components/TDemo'
+	import RadioSpecial from "./components/RadioSpecial"
+
+	export default defineComponent({
+		name: "DemoRadio",
+		components: {
+			TDemo,
+			RadioSpecial
+		},
+		setup() {
+			const curRadio = ref('3');
+			const options = ref([{
+					value: 0,
+					label: '单选'
+				},
+				{
+					value: 1,
+					label: '单选'
+				},
+				{
+					value: 2,
+					label: '单选单选单选单选单选单选单选单选单选单选单选单选单选单选'
+				},
+				{
+					value: 3,
+					label: '单选',
+					content: '描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息',
+				},
+			]);
+			const groupChangeFn = (value, context) => {
+				console.log(value, context);
+			};
+
+			const radio1 = ref('1');
+
+			const statusOptions = [{
+					value: '1',
+					label: '选项禁用-已选',
+				},
+				{
+					value: '2',
+					label: '选项禁用-默认',
+				},
+			];
+
+
+			const radio2 = ref('1');
+
+			return {
+				curRadio,
+				options,
+				groupChangeFn,
+				radio1,
+				statusOptions,
+				radio2
+			}
+		}
+	})
+</script>
+
+<style scoped>
+	.t-radio-padding {
+		padding: 24rpx 32rpx !important;
+	}
+
+	.t-radio-demo {
+		margin-top: 16px;
+	}
+
+	.disable-border {
+		display: none;
+	}
+
+	.boxRadioWrap {
+		padding: 32rpx;
+		display: flex;
+		justify-content: space-between;
+		background-color: #fff;
+	}
+
+	.theme-card {
+		border-radius: 24rpx;
+		margin: 32rpx;
+		overflow: hidden;
+	}
+</style>
